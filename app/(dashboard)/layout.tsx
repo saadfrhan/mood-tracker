c8e3d1f69665de435868c30f423af5834b2f4dbf
saadfrhan <saadfarhan347@gmail.com>
2025-04-07 09:26:14 +0500

    remove swipe navigation

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,6 @@
 import type React from "react";
 import { Sidebar } from "@/components/sidebar";
 import { MobileNav } from "@/components/mobile-nav";
-// Import the SwipeNavigation component
-import { SwipeNavigation } from "@/components/swipe-navigation";
-import { SwipeIndicator } from "@/components/swipe-indicator";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
 // Wrap the content with SwipeNavigation
@@ -17,8 +14,6 @@ export default function DashboardLayout({
       <SidebarProvider>
         <Sidebar />
         <div className="flex-1 flex flex-col">
-          <SwipeNavigation>{children}</SwipeNavigation>
-          <SwipeIndicator />
           <MobileNav />
         </div>
       </SidebarProvider>
